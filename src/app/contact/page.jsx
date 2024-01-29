8a12a6401364d8bbf244a250b2ef0a8a3df4722b
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-29 23:00:45 +0600

    Style the contact form page.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
     return (
-        <div>
-            <h2>Contact Page</h2>
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <Image src="/contact.png" alt=" " className={styles.img} fill/>
+            </div>
+            <div className={styles.formContainer}>
+                <form action="server action" className={styles.form}>
+                    <input type="text" placeholder="Name and Surname" name="" />
+                    <input type="text" placeholder="Email address" name="" />
+                    <input type="text" placeholder="Phone Number (Optional)" name="" />
+                    <textarea
+                        name=""
+                        id=""
+                        cols={30}
+                        rows={10}
+                        placeholder=""
+                    ></textarea>
+                    <button>Send</button>
+                </form>
+            </div>
         </div>
     );
 };
