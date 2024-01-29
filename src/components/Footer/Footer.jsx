80449009328363204f8d95b514bb142de80f93b8
Ricky Ponting <qarimahdi217@gmail.com>
2024-01-29 20:37:52 +0600

    Style the footer section.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 
+import styles from "./Footer.module.css"
+
 const Footer = () => {
     return (
-        <div>
-            Footer      
+        <div className={styles.container}>
+            <div className={styles.logo}>lamadev</div>      
+            <div className={styles.text}>lama creative agency. all rights reserved.</div>
         </div>
     );
 };
